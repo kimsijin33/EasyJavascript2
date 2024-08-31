d3618c10b4933f214b7c3c3950c0dd4ea301ee77
KimSiJin <kimsijin33@gmail.com>
2024-08-31 13:07:44 +0900

    ~p33

---
+++ p30.js
@@ -0,0 +1,4 @@
+
+var 고칠문자열 = "hELlo THERE, hOW ARE yOudoINg?";
+고칠문자열[0].toUpperCase() + 고칠문자열.slice(1).toLowerCase();
+
---
+++ p33.js
@@ -0,0 +1,38 @@
+
+var 샤워하기 = true;
+undefined
+var 가방챙기기 = false;
+undefined
+샤워하기 && 가방챙기기;
+false
+var 가방챙기기 = true;
+undefined
+샤워하기 && 가방챙기기;
+true
+var 사과챙기기 = true;
+undefined
+var 오렌지챙기기 = false;
+undefined
+사과챙기기 || 오렌지챙기기;
+true
+var 주말인가요 = true;
+undefined
+var 샤워해야하나요 = !주말인가요;
+undefined
+샤워해야하나요
+false
+!샤워해야하나요
+true
+var 주말인가요 = false;
+undefined
+var 샤워하기 = true;
+undefined
+    var 사과챙기기 = false;
+undefined
+var 오렌지챙기기 = true;
+undefined
+var 나가야하나요 = !주말인가요 && 샤워하기 && (사과챙기기 || 오렌지챙기기);
+undefined
+나가야하나요
+true
+
